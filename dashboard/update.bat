37e1cebd0d2a6f59c36634347fa471fffde813c7
Guillaume RYCKELYNCK <guillaume.ryckelynck@grandest.fr>
2023-09-20 22:58:47 +0200

    move CSV files to 'data' directory

--- dashboard/update.bat
+++ dashboard/update.bat
@@ -1,16 +1,16 @@
 REM WMS
-..\output\report\report.exe reports 0 --csv wms_report.csv
-..\output\report\report.exe errors 0 --csv wms_errors.csv
-..\output\report\report.exe layers 0 --csv wms_layers.csv
-..\output\report\report.exe ws 0 --csv wms_ws.csv
+..\output\report\report.exe reports 0 --csv ./data/wms_report.csv
+..\output\report\report.exe errors 0 --csv ./data/wms_errors.csv
+..\output\report\report.exe layers 0 --csv ./data/wms_layers.csv
+..\output\report\report.exe ws 0 --csv ./data/wms_ws.csv
 
 REM WFS
-..\output\report\report.exe reports 1 --csv wfs_report.csv
-..\output\report\report.exe errors 1 --csv wfs_errors.csv
-..\output\report\report.exe layers 1 --csv wfs_layers.csv
-..\output\report\report.exe ws 1 --csv wfs_ws.csv
+..\output\report\report.exe reports 1 --csv ./data/wfs_report.csv
+..\output\report\report.exe errors 1 --csv ./data/wfs_errors.csv
+..\output\report\report.exe layers 1 --csv ./data/wfs_layers.csv
+..\output\report\report.exe ws 1 --csv ./data/wfs_ws.csv
 
 REM CSW
-..\output\report\report.exe reports 2 --csv csw_report.csv
-..\output\report\report.exe errors 2 --csv csw_errors.csv
-..\output\report\report.exe layers 2 --csv csw_layers.csv
+..\output\report\report.exe reports 2 --csv ./data/csw_report.csv
+..\output\report\report.exe errors 2 --csv ./data/csw_errors.csv
+..\output\report\report.exe layers 2 --csv ./data/csw_layers.csv
